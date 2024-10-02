a67d5364a420b4cdd5484123c33ab8aa04887dd9
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-10-02 11:21:26 +0200

    feat(#10): restaurants can add menu items

--- src/main/java/fr/unice/polytech/steats/restaurant/Restaurant.java
+++ src/main/java/fr/unice/polytech/steats/restaurant/Restaurant.java
@@ -16,7 +16,16 @@ public record Restaurant(String name, List<MenuItem> menu, TypeOfFood typeOfFood
         this.typeOfFood = typeOfFood;
     }
 
+    // TODO : filter the menu according to deliveryTime
     public List<MenuItem> getAvailableMenu(LocalDateTime deliveryTime) {
-        return this.menu;
+        return new ArrayList<>(this.menu);
+    }
+
+    public List<MenuItem> getMenu() {
+        return new ArrayList<>(this.menu);
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        this.menu.add(menuItem);
     }
 }
