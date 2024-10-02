2ac8e80c4f0a38da4028c9c2b836e7d06780a420
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-10-02 09:15:42 +0200

    docs(#2): Discount description

--- src/main/java/fr/unice/polytech/steats/discounts/Discount.java
+++ src/main/java/fr/unice/polytech/steats/discounts/Discount.java
@@ -6,6 +6,12 @@ import fr.unice.polytech.steats.restaurant.MenuItem;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * A restaurant's discount with trigger criteria.
+ *
+ * @author Team C
+ * @see DiscountBuilder
+ */
 public class Discount {
     private final DiscountBuilder.Options options;
     private final DiscountBuilder.Criteria criteria;
@@ -17,10 +23,15 @@ public class Discount {
         this.discounts = builder.getDiscounts();
     }
 
+    /**
+     * Checks if the criteria are met
+     * @param order The current order
+     * @return True if the discount can be applied to the order
+     */
     public boolean isApplicable(Order order) {
         List<MenuItem> items = order.getItems();
         // TODO: Other criteria need the User class
         return options.expirationDate.isAfter(LocalDateTime.now())
-        && items.size() >= criteria.currentOrderItemsAmount;
+                && items.size() >= criteria.currentOrderItemsAmount;
     }
 }
