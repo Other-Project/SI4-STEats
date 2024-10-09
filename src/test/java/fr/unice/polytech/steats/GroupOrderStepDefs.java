9cdc7d9183759da16c9c3578adfc683906e026f0
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2024-10-09 10:42:08 +0200

    test(#16): Added more test

--- src/test/java/fr/unice/polytech/steats/GroupOrderStepDefs.java
+++ src/test/java/fr/unice/polytech/steats/GroupOrderStepDefs.java
@@ -1,8 +1,10 @@
 package fr.unice.polytech.steats;
 
 import fr.unice.polytech.steats.order.Address;
+import fr.unice.polytech.steats.order.GroupOrder;
 import fr.unice.polytech.steats.order.GroupOrderManager;
 import fr.unice.polytech.steats.order.SingleOrder;
+import fr.unice.polytech.steats.restaurant.MenuItem;
 import fr.unice.polytech.steats.restaurant.Restaurant;
 import fr.unice.polytech.steats.user.Role;
 import fr.unice.polytech.steats.user.User;
@@ -11,20 +13,27 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class GroupOrderStepDefs {
 
+    private User user;
+    private STEats steatsAlex;
+
     @Given("A group order is created with the group code {string} and the delivery time {string} and the address {string} and the restaurant {string}")
     public void a_group_order_is_created(String groupCode, String deliveryTime, String address, String restaurant) {
-        STEats steats = new STEats(new User("Alban", "12345", Role.EXTERNAL));
-        steats.createGroupOrder(groupCode, LocalDateTime.parse(deliveryTime), new Address(address, "Antibes", "06600", "villa_116"), new Restaurant(restaurant));
+        GroupOrderManager.addGroupOrder(new GroupOrder(groupCode, LocalDateTime.parse(deliveryTime), new Address(address, "1", "1", "1"), new Restaurant(restaurant)));
+    }
+
+    @Given("The user with the id {string} is logged in")
+    public void theUserWithTheIdIsLoggedIn(String userId) {
+        user = new User("Alex", userId, Role.STUDENT);
+        steatsAlex = new STEats(user);
     }
 
     @When("The user with the id {string} joins the group order with the group code {string}")
     public void the_user_joins_the_group_order(String userId, String groupCode) {
-        User user = new User("Alex", userId, Role.STUDENT);
-        STEats steats = new STEats(user);
-        steats.joinGroupOrder(groupCode);
+        steatsAlex.joinGroupOrder(groupCode);
     }
 
     @Then("The user with the id {string} is added to the group order with the group code {string}")
@@ -36,4 +45,26 @@ public class GroupOrderStepDefs {
                 .toList()
                 .contains(userId);
     }
+
+    @When("The user with the id {string} adds the item named {string} with a price of {double} to the group order with the group code {string}")
+    public void theUserWithTheIdAddsTheItemNamedWithAPriceOfToTheGroupOrderWithTheGroupCode(String id, String menuItem, double price, String arg4) {
+        steatsAlex.addMenuItem(new MenuItem(menuItem, price, LocalTime.of(12, 0)));
+    }
+
+    @Then("The item with named {string} is added to the order of the user with the id {string} in the group order with the group code {string}")
+    public void theItemWithNamedIsAddedToTheOrderOfTheUserWithTheIdInTheGroupOrderWithTheGroupCode(String arg0, String arg1, String arg2) {
+        assert steatsAlex.getTotalPrice() == 10.0;
+        assert GroupOrderManager.getGroupOrder(arg2).getOrders().stream()
+                .map(order -> order.getItems().size())
+                .toList()
+                .contains(1);
+        assert GroupOrderManager.getGroupOrder(arg2).getOrders().stream()
+                .map(order -> order.getItems().getFirst().getName())
+                .toList()
+                .contains(arg0);
+        assert GroupOrderManager.getGroupOrder(arg2).getOrders().stream()
+                .map(order -> ((SingleOrder) order).getUserId())
+                .toList().contains(arg1);
+    }
+
 }
--- src/test/resources/features.steats/GroupOrder.feature
+++ src/test/resources/features.steats/GroupOrder.feature
@@ -2,11 +2,18 @@
 Feature: Manage GroupOrder
 
   Background:
-    Given A group order is created with the group code "groupCode" and the delivery time "2019-03-27T10:15:30" and the address "292 Chemin de la Rigolade" and the restaurant "McDonald's"
+    Given The user with the id "01234" is logged in
 
   Scenario: Join GroupOrder
-    When The user with the id "01234" joins the group order with the group code "groupCode"
-    Then The user with the id "01234" is added to the group order with the group code "groupCode"
+    Given A group order is created with the group code "groupCode1" and the delivery time "2019-03-27T10:15:30" and the address "292 Chemin de la Rigolade" and the restaurant "McDonald's"
+    When The user with the id "01234" joins the group order with the group code "groupCode1"
+    Then The user with the id "01234" is added to the group order with the group code "groupCode1"
+
+  Scenario: Add Item to GroupOrder
+    Given A group order is created with the group code "groupCode2" and the delivery time "2019-03-27T10:15:30" and the address "292 Chemin de la Rigolade" and the restaurant "McDonald's"
+    When The user with the id "01234" joins the group order with the group code "groupCode2"
+    When The user with the id "01234" adds the item named "BigMac" with a price of 10.0 to the group order with the group code "groupCode2"
+    Then The item with named "BigMac" is added to the order of the user with the id "01234" in the group order with the group code "groupCode2"
 
 
 
