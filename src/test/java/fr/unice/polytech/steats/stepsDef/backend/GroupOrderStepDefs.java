1929493ad9253f94932387600485d2371c494765
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2024-10-16 17:16:34 +0200

    test(#33): Added test for closing and validating group orders

--- src/test/java/fr/unice/polytech/steats/stepsDef/backend/GroupOrderStepDefs.java
+++ src/test/java/fr/unice/polytech/steats/stepsDef/backend/GroupOrderStepDefs.java
@@ -1,26 +1,38 @@
 package fr.unice.polytech.steats.stepsDef.backend;
 
 import fr.unice.polytech.steats.STEats;
-import fr.unice.polytech.steats.order.Address;
-import fr.unice.polytech.steats.order.GroupOrder;
-import fr.unice.polytech.steats.order.GroupOrderManager;
-import fr.unice.polytech.steats.order.SingleOrder;
+import fr.unice.polytech.steats.STEatsController;
+import fr.unice.polytech.steats.order.*;
 import fr.unice.polytech.steats.restaurant.MenuItem;
 import fr.unice.polytech.steats.restaurant.Restaurant;
 import fr.unice.polytech.steats.user.NotFoundException;
 import fr.unice.polytech.steats.user.Role;
 import fr.unice.polytech.steats.user.User;
 import fr.unice.polytech.steats.user.UserManager;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class GroupOrderStepDefs {
 
-    private STEats steats;
+    STEatsController stEatsController = new STEatsController();
+    Map<String, STEats> steatsMap = new HashMap<>();
+
+    @Before
+    public void before() {
+        GroupOrderManager.getInstance().clear();
+        UserManager.getInstance().clear();
+    }
 
     @Given("A group order with the group code {string} from the restaurant {string} and to deliver for {string} at {string}")
     public void a_group_order_is_created(String groupCode, String restaurant, String deliveryTime, String address) {
@@ -28,46 +40,88 @@ public class GroupOrderStepDefs {
     }
 
     @Given("The user named {string} with the id {string} is logged in")
-    public void theUserWithTheIdIsLoggedIn(String name, String userId) {
+    public void theUserWithTheIdIsLoggedIn(String name, String userId) throws NotFoundException {
         User user = new User(name, userId, Role.STUDENT);
         UserManager.getInstance().add(userId, user);
-        steats = new STEats(user);
+        steatsMap.put(userId, stEatsController.logging(userId));
     }
 
-    @When("The user joins the group order with the group code {string}")
-    public void the_user_joins_the_group_order(String groupCode) throws NotFoundException {
-        steats.joinGroupOrder(groupCode);
+    @When("The user with the id {string} joins the group order with the group code {string}")
+    public void the_user_joins_the_group_order(String userId, String groupCode) throws NotFoundException {
+        steatsMap.get(userId).joinGroupOrder(groupCode);
     }
 
     @Then("The user with the id {string} is added to the group order with the group code {string}")
     public void the_user_is_added_to_the_group_order(String userId, String groupCode) throws NotFoundException {
-        assert GroupOrderManager.getInstance().get(groupCode).getOrders().size() == 1;
-        assert GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
+        assertEquals(1, GroupOrderManager.getInstance().get(groupCode).getOrders().size());
+        assertTrue(GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
                 .filter(order -> order instanceof SingleOrder)
                 .map(order -> ((SingleOrder) order).getUserId())
                 .toList()
-                .contains(userId);
+                .contains(userId));
     }
 
-    @When("The user adds the item named {string} with a price of {double} to the group order")
-    public void theUserWithTheIdAddsTheItemNamedWithAPriceOfToTheGroupOrderWithTheGroupCode(String menuItem, double price) {
-        steats.addMenuItem(new MenuItem(menuItem, price, Duration.ofMinutes(10)));
+    @When("The user with the id {string} adds the item named {string} with a price of {double} to the group order")
+    public void theUserWithTheIdAddsTheItemNamedWithAPriceOfToTheGroupOrderWithTheGroupCode(String userId, String menuItem, double price) {
+        steatsMap.get(userId).addMenuItem(new MenuItem(menuItem, price, Duration.ofMinutes(10)));
     }
 
     @Then("The item with named {string} with a price of {double} is added to the order of the user with the id {string} in the group order with the group code {string}")
-    public void theItemWithNamedIsAddedToTheOrderOfTheUserWithTheIdInTheGroupOrderWithTheGroupCode(String menuItem, Double price, String userId, String groupCode) throws NotFoundException {
-        assert steats.getTotalPrice() == price;
-        assert GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
+    public void theItemWithNamedIsAddedToTheOrderOfTheUserWithTheIdInTheGroupOrderWithTheGroupCode(String menuItem, double price, String userId, String groupCode) throws NotFoundException {
+        assertEquals(price, steatsMap.get(userId).getTotalPrice(), 0.1);
+        assertTrue(GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
                 .map(order -> order.getItems().size())
                 .toList()
-                .contains(1);
-        assert GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
+                .contains(1));
+        assertTrue(GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
                 .map(order -> order.getItems().getFirst().getName())
                 .toList()
-                .contains(menuItem);
-        assert GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
+                .contains(menuItem));
+        assertTrue(GroupOrderManager.getInstance().get(groupCode).getOrders().stream()
                 .map(order -> ((SingleOrder) order).getUserId())
-                .toList().contains(userId);
+                .toList().contains(userId));
     }
 
+    @When("The user with the id {string} pay")
+    public void theUserWithTheIdPay(String userId) {
+        steatsMap.get(userId).payOrder();
+    }
+
+    @Then("The order of the user with the id {string} is payed")
+    public void theOrderOfTheUserWithTheIdIsPayed(String userId) {
+        assertSame(Status.PAID, steatsMap.get(userId).getOrder().getStatus());
+    }
+
+    @And("The order is added to the history of the user with the id {string}")
+    public void theOrderIsAddedToTheHistoryOfTheUserWithTheId(String userId) throws NotFoundException {
+        assertEquals(1, UserManager.getInstance().get(userId).getOrders().size());
+    }
+
+    @And("The group order with the group code {string} is payed")
+    public void theGroupOrderIsPayed(String groupCode) throws NotFoundException {
+        assertSame(Status.PAID, GroupOrderManager.getInstance().get(groupCode).getStatus());
+    }
+
+    @And("The user with the id {string} can close the group order")
+    public void heCanCloseTheGroupOrder(String userId) throws NotFoundException {
+        assertTrue(steatsMap.get(userId).canCloseGroupOrder());
+        assertDoesNotThrow(() -> steatsMap.get(userId).closeGroupOrder());
+    }
+
+
+    @And("The user with the id {string} can't close the group order")
+    public void theUserWithTheIdCanTCloseTheGroupOrder(String userId) throws NotFoundException {
+        assertFalse(steatsMap.get(userId).canCloseGroupOrder());
+        assertThrows(IllegalStateException.class, () -> steatsMap.get(userId).closeGroupOrder());
+    }
+
+    @When("The user with the id {string} close the group order")
+    public void theUserWithTheIdCloseTheGroupOrder(String userId) throws NotFoundException {
+        steatsMap.get(userId).closeGroupOrder();
+    }
+
+    @Then("the group order with the id {string} is closed")
+    public void theGroupOrderIsClosed(String groupeCode) throws NotFoundException {
+        assertSame(Status.PAID, GroupOrderManager.getInstance().get(groupeCode).getStatus());
+    }
 }
--- src/test/resources/features.steats/GroupOrder.feature
+++ src/test/resources/features.steats/GroupOrder.feature
@@ -3,15 +3,54 @@ Feature: Manage GroupOrder
 
   Background:
     Given The user named "Alex" with the id "1234" is logged in
+    Given The user named "Alban" with the id "4321" is logged in
+    Given A group order with the group code "groupCode2" from the restaurant "McDonald's" and to deliver for "2024-03-29T10:15:30" at "292 Chemin de la Rigolade"
 
   Scenario: Join GroupOrder
-    Given A group order with the group code "groupCode1" from the restaurant "McDonald's" and to deliver for "2024-03-27T10:15:30" at "292 Chemin de la Rigolade"
-    When The user joins the group order with the group code "groupCode1"
-    Then The user with the id "1234" is added to the group order with the group code "groupCode1"
+    When The user with the id "1234" joins the group order with the group code "groupCode2"
+    Then The user with the id "1234" is added to the group order with the group code "groupCode2"
 
   Scenario: Add Item to GroupOrder
-    Given A group order with the group code "groupCode2" from the restaurant "McDonald's" and to deliver for "2024-03-29T10:15:30" at "292 Chemin de la Rigolade"
-    When The user joins the group order with the group code "groupCode2"
-    When The user adds the item named "bigmac" with a price of 10.0 to the group order
+    When The user with the id "1234" joins the group order with the group code "groupCode2"
+    When The user with the id "1234" adds the item named "bigmac" with a price of 10.0 to the group order
     Then The item with named "bigmac" with a price of 10.0 is added to the order of the user with the id "1234" in the group order with the group code "groupCode2"
+
+  Scenario: Close Group Order
+    Given The user with the id "1234" joins the group order with the group code "groupCode2"
+    Given The user with the id "4321" joins the group order with the group code "groupCode2"
+    Given The user with the id "1234" adds the item named "bigmac" with a price of 10.0 to the group order
+    Given The user with the id "4321" adds the item named "bigmac++" with a price of 20.0 to the group order
+    When The user with the id "1234" pay
+    Then The order of the user with the id "1234" is payed
+    And The user with the id "1234" can't close the group order
+
+  Scenario: Can close Group Order completely
+    Given The user with the id "1234" joins the group order with the group code "groupCode2"
+    Given The user with the id "4321" joins the group order with the group code "groupCode2"
+    Given The user with the id "1234" adds the item named "bigmac" with a price of 10.0 to the group order
+    Given The user with the id "4321" adds the item named "bigmac++" with a price of 20.0 to the group order
+    Given The user with the id "1234" pay
+    When The user with the id "4321" pay
+    Then The order of the user with the id "4321" is payed
+    And The user with the id "4321" can close the group order
+
+  Scenario: Close Group Order completely
+    Given The user with the id "1234" joins the group order with the group code "groupCode2"
+    Given The user with the id "4321" joins the group order with the group code "groupCode2"
+    Given The user with the id "1234" adds the item named "bigmac" with a price of 10.0 to the group order
+    Given The user with the id "4321" adds the item named "bigmac++" with a price of 20.0 to the group order
+    Given The user with the id "1234" pay
+    Given The user with the id "4321" pay
+    When The user with the id "1234" close the group order
+    Then the group order with the id "groupCode2" is closed
+    And The order is added to the history of the user with the id "1234"
+    And The order is added to the history of the user with the id "4321"
+    And The user with the id "1234" can't close the group order
+    And The user with the id "4321" can't close the group order
+
+
+
+
+
+
 
