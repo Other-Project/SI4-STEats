fe48e4214aea3f08a6acc18c3c70f31fe4001580
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2024-10-16 10:04:55 +0200

    test(#33): Added payment system

---
+++ src/main/java/fr/unice/polytech/steats/PaymentSystem.java
@@ -0,0 +1,19 @@
+package fr.unice.polytech.steats;
+
+/**
+ * Represents the payment system.
+ *
+ * @author Team C
+ */
+public class PaymentSystem {
+
+    /**
+     * Pay the order
+     *
+     * @param amount the amount to pay
+     * @return if the payment was successful
+     */
+    public static boolean pay(double amount) {
+        return true;
+    }
+}
