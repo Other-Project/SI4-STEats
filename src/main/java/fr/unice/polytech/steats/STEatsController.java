0dd2311f2c0f281ae1e25f1cd4ebaa89e07564cb
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-10-21 14:08:26 +0200

    refactor(#19): removed unnecessary try/catch

--- src/main/java/fr/unice/polytech/steats/STEatsController.java
+++ src/main/java/fr/unice/polytech/steats/STEatsController.java
@@ -1,7 +1,6 @@
 package fr.unice.polytech.steats;
 
 import fr.unice.polytech.steats.user.NotFoundException;
-import fr.unice.polytech.steats.user.User;
 import fr.unice.polytech.steats.user.UserManager;
 
 /**
@@ -16,11 +15,6 @@ public class STEatsController {
      * @return the facade associated with  the user
      */
     public STEats logging(String userId) throws NotFoundException {
-        try {
-            User user = UserManager.getInstance().get(userId);
-            return new STEats(user);
-        } catch (NotFoundException e) {
-            throw new NotFoundException("User " + userId + " not found");
-        }
+        return new STEats(UserManager.getInstance().get(userId));
     }
 }
