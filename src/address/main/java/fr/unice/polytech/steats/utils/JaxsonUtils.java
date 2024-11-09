f76d94b5410cf648e0907c9f2c446662cd401632
Evan Galli <evan_g@orange.fr>
2024-11-09 14:25:41 +0100

    feat(#66): Get payments by user or order

--- src/address/main/java/fr/unice/polytech/steats/utils/JaxsonUtils.java
+++ src/address/main/java/fr/unice/polytech/steats/utils/JaxsonUtils.java
@@ -1,29 +1,41 @@
 package fr.unice.polytech.steats.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class JaxsonUtils {
     private JaxsonUtils() {
     }
 
-    public static String toJson(Object object) throws JsonProcessingException {
+    private static ObjectMapper getMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.findAndRegisterModules();
-        return mapper.writeValueAsString(object);
+        return mapper;
+    }
+
+    public static String toJson(Object object) throws JsonProcessingException {
+        return getMapper().writeValueAsString(object);
     }
 
     public static <T> T fromJson(InputStream json, Class<T> clazz) throws IOException {
-        return fromJson(new String(json.readAllBytes(), StandardCharsets.UTF_8), clazz);
+        return fromJson(new String(json.readAllBytes(), StandardCharsets.UTF_8), TypeFactory.defaultInstance().constructType(clazz));
     }
 
-    public static <T> T fromJson(String json, Class<T> clazz) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
-        return mapper.readValue(json, clazz);
+    public static <T extends Map<K, V>, K, V> T fromJson(InputStream json) throws IOException {
+        return fromJson(new String(json.readAllBytes(), StandardCharsets.UTF_8), TypeFactory.defaultInstance().constructType(new TypeReference<T>() {
+        }));
+    }
+
+    public static <T> T fromJson(String json, JavaType type) throws JsonProcessingException {
+        if (json == null || json.isBlank()) return null;
+        return getMapper().readValue(json, type);
     }
 }
--- src/payments/main/java/fr/unice/polytech/steats/payments/PaymentsHttpHandler.java
+++ src/payments/main/java/fr/unice/polytech/steats/payments/PaymentsHttpHandler.java
@@ -1,8 +1,6 @@
 package fr.unice.polytech.steats.payments;
 
 import com.sun.net.httpserver.HttpExchange;
-import fr.unice.polytech.steats.users.User;
-import fr.unice.polytech.steats.users.UserManager;
 import fr.unice.polytech.steats.utils.AbstractManagerHandler;
 import fr.unice.polytech.steats.utils.ApiRegistry;
 import fr.unice.polytech.steats.utils.HttpUtils;
@@ -13,25 +11,36 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
 
-public class PaymentsHttpHandler extends AbstractManagerHandler<UserManager, User> {
+public class PaymentsHttpHandler extends AbstractManagerHandler<PaymentManager, Payment> {
     public PaymentsHttpHandler(String subPath, Logger logger) {
-        super(subPath, User.class, logger);
+        super(subPath, Payment.class, logger);
     }
 
     @Override
-    protected UserManager getManager() {
-        return UserManager.getInstance();
+    protected PaymentManager getManager() {
+        return PaymentManager.getInstance();
     }
 
     @Override
     protected void register() {
-        ApiRegistry.registerRoute("GET", getSubPath() + "/{id}", this::get);
-        ApiRegistry.registerRoute("GET", getSubPath(), (exchange, param) -> getAll(exchange));
-        ApiRegistry.registerRoute("PUSH", getSubPath() + "/pay", (exchange, param) -> pay(exchange, HttpUtils.parseQuery(exchange.getRequestURI().getQuery())));
+        ApiRegistry.registerRoute(HttpUtils.GET, getSubPath() + "/{id}", super::get);
+        ApiRegistry.registerRoute(HttpUtils.GET, getSubPath(), (exchange, param) -> getAll(exchange, HttpUtils.parseQuery(exchange.getRequestURI().getQuery())));
+        ApiRegistry.registerRoute(HttpUtils.POST, getSubPath() + "/pay", (exchange, param) -> pay(exchange));
     }
 
-    private void pay(HttpExchange exchange, Map<String, String> params) throws IOException {
-        String orderId = params.get("orderId");
+    private void getAll(HttpExchange exchange, Map<String, String> params) throws IOException {
+        String orderId;
+        String userId;
+        if ((orderId = params.get("orderId")) != null)
+            HttpUtils.sendJsonResponse(exchange, HttpUtils.OK_CODE, getManager().getPaymentsForOrder(orderId));
+        else if ((userId = params.get("userId")) != null)
+            HttpUtils.sendJsonResponse(exchange, HttpUtils.OK_CODE, getManager().getPaymentsOfUser(userId));
+        else HttpUtils.sendJsonResponse(exchange, HttpUtils.OK_CODE, getManager().getAll());
+    }
+
+    private void pay(HttpExchange exchange) throws IOException {
+        Map<String, Object> params = JaxsonUtils.fromJson(exchange.getRequestBody());
+        String orderId = params == null ? null : params.get("orderId").toString();
         if (orderId == null) {
             exchange.sendResponseHeaders(HttpUtils.BAD_REQUEST_CODE, 0);
             exchange.getResponseBody().close();
