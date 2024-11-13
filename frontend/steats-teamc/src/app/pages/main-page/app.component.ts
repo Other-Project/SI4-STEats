5264974f594173e15f2cdea259d244b55bc3ae5f
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-11-13 11:38:46 +0100

    feat(#75): added the main page

--- frontend/steats-teamc/src/app/pages/main-page/app.component.ts
+++ frontend/steats-teamc/src/app/pages/main-page/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {AppModule} from '../../app.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
