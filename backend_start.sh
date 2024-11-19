8c7fa50706838185107ef8eda1491505bdd76e5d
Evan Galli <evan_g@orange.fr>
2024-11-19 15:13:27 +0100

    feat(#78): Script to automatically launch all servers

---
+++ backend_start.sh
@@ -0,0 +1,13 @@
+﻿trap terminate SIGINT
+terminate(){
+    pkill -SIGINT -P $$
+    exit
+}
+
+cd backend
+mvn -f common/pom.xml clean install -Dmaven.test.skip=true
+for dir in ./*/
+do
+  [dir != "./common/"] && mvn -f $dir/pom.xml clean compile exec:java -Dmaven.test.skip=true &
+fi
+wait
